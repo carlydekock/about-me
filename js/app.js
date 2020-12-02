f9eab1e056be713878cd8db8851013601199f8f7
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2020-12-01 18:34:07 -0800

    Finalized promps and commented out console.log messages

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var userName = prompt('What is your name?');
-console.log('The user name is ' + userName + '!');
+// console.log('The user name is ' + userName + '!');
 
 //TODO - create personalized welcome message with userName
 //document.write('Hi ' + userName + ', thanks for visiting my site!');
@@ -12,7 +12,7 @@ alert('Hi ' + userName + ', thanks for visiting my site!');
 var homeTown = prompt('Am I from Seattle?').toLowerCase();
 //TODO - console.log the answer if it is correct, in a string concatenation
 if (homeTown === 'yes' || homeTown === 'y'){
-  console.log(userName + ' says ' + homeTown + ', I live in Seattle.');
+  // console.log(userName + ' says ' + homeTown + ', I live in Seattle.');
   alert('That\'s correct!');
 } else {
   alert('You\'re wrong, I am from Seattle.');
@@ -24,7 +24,7 @@ if (homeTown === 'yes' || homeTown === 'y'){
 var myAge = prompt('Am I 30 years old?').toLowerCase();
 //TODO - console.log the answer if it is correct, in a string concatenation
 if (myAge === 'yes' || myAge === 'y'){
-  console.log(userName + ' answered ' + myAge + ', I am 30 years old.');
+  // console.log(userName + ' answered ' + myAge + ', I am 30 years old.');
   alert('That\'s correct!');
 } else {
   alert('You\'re wrong, I am 30 years old.');
@@ -37,7 +37,7 @@ if (myAge === 'yes' || myAge === 'y'){
 var favoriteHobby = prompt('Do I enjoy skiing?').toLowerCase();
 //TODO - console.log the answer if it is correct, in a string concatenation
 if (favoriteHobby === 'yes' || favoriteHobby === 'y'){
-  console.log(userName + ' answered correctly, I do enjoy skiing.');
+  // console.log(userName + ' answered correctly, I do enjoy skiing.');
   alert('You are correct, I do!');
 } else {
   alert('You are wrong, I do enjoy it!');
@@ -50,7 +50,7 @@ if (favoriteHobby === 'yes' || favoriteHobby === 'y'){
 var favoriteAnimal = prompt('Are cats my favorite animal?').toLowerCase();
 //TODO - console.log the answer if it is correct, in a string concatenation
 if (favoriteAnimal === 'no' || favoriteAnimal === 'n'){
-  console.log('Great job ' + userName + ', you are right! Dogs are better!');
+  // console.log('Great job ' + userName + ', you are right! Dogs are better!');
   alert('You\'re right, dogs are better!');
 } else {
   alert('You are wrong, I like dogs better.');
@@ -64,7 +64,7 @@ if (favoriteAnimal === 'no' || favoriteAnimal === 'n'){
 var favoriteSeason = prompt('Is summer my favorite season?').toLowerCase();
 //TODO - console.log the answer if it is correct, in a string concatenation
 if (favoriteSeason === 'yes' || favoriteSeason === 'y'){
-  console.log('Right you are ' + userName + '! I do love summer.');
+  // console.log('Right you are ' + userName + '! I do love summer.');
   alert('That\'s correct!');
 } else {
   alert('Nope, you are wrong.');
@@ -72,13 +72,3 @@ if (favoriteSeason === 'yes' || favoriteSeason === 'y'){
 //TODO - if it works, comment out console.log, send alert to user
 
 alert('Thanks for answering all these questions ' + userName + ', I appreciate you wanting to know a bit more about me!');
-
-
-// var favoriteFood = prompt('What is your favorite food?');
-// console.log(userName + ' answered that ' + favoriteFood + ' is their favorite food.');
-
-// var feelingToday = prompt('How do you feel today?');
-// console.log(userName + ' feels ' + feelingToday + ' today.');
-
-// alert('Thanks for answering ' + userName + ', ' + homeTown + ' is great!' + ' I really enjoy ' + favoriteFood + ' as well. I appreciate you letting me know that you feel ' + feelingToday + ' today. It is always good to check in on each other.');
-
